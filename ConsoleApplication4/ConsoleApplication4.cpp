10081e7285dfe36f3fbcc4265ea98b1cf1feac53
Александр Капара <a.kapara2006@gmail.com>
2022-05-10 19:40:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	SYSTEMTIME st;
+	while (true) 
+	{
+		GetLocalTime(&st);
+		cout << "Days: " << st.wDay << "\n" << "Month: " << st.wMonth << "\n" << "Year: " << st.wYear << "\n";
+		cout << "Hours: " << st.wHour << "\n" << "Minutes: " << st.wMinute << "\n" << "Seconds: " << st.wSecond << "\n";
+
+		Sleep(1000);
+		system("cls");
+
+	}
+}
